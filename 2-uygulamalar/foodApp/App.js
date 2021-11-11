310bcc663af621c5a457dca8c85dd548a93577aa
hilal <62089994+ayperi-hilal@users.noreply.github.com>
2021-11-11 16:48:16 +0300

    Add files via upload

--- 2-uygulamalar/foodApp/App.js
+++ 2-uygulamalar/foodApp/App.js
@@ -5,11 +5,34 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Yemek Tarifi Uygulamamıza Hoş geldiniz </Text>,
       <StatusBar style="auto" />
     </View>
+    
+
   );
 }
+
+const menu=()=>{
+        <SafeAreaView>
+         <TouchableOpacity style={styles.row}>
+       
+         <Text style={styles.isim}> tarif adı</Text>
+        </TouchableOpacity>
+
+        <View style={styles.cizgi}/>
+        
+        <TouchableOpacity style={styles.row}>
+       
+       <Text style={styles.isim}> tarif adı</Text>
+      </TouchableOpacity>
+
+        <View style={styles.cizgi}/>
+
+        </SafeAreaView>
+
+}
+ 
 
 const styles = StyleSheet.create({
   container: {
@@ -17,5 +40,22 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  isim:{
+
+    color:'red',
+    
+  },
+  cizgi:{
+        height:StyleSheet.hairlineWidth,
+        /*bu kısımda yüksekliğe bi sayı değerii verebilirdik fakat ekran ekrana değer değişimi olacağı için en küçük değeri bu şekilde verdik.*/ 
+        backgroundColor:'#e2e2e2',
+        marginStart:16
+  },
+
+  row:{
+    paddingHorizontal:16,/*hem sağdan hemdesoldan boşluk bırakacaktır.*/
+    paddingVertical:20,/*hem üstten hemde alttan boşluk bırakacaktır.*/
+
   },
 });
