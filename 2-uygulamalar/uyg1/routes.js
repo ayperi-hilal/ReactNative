2134b9fda75daa478e3101513b4122523468da39
hilal <62089994+ayperi-hilal@users.noreply.github.com>
2021-11-18 17:58:42 +0300

    Add files via upload

--- 2-uygulamalar/uyg1/routes.js
+++ 2-uygulamalar/uyg1/routes.js
@@ -1,10 +1,10 @@
-import {createAppContainer} from 'react-navigation';
-import {createStackNaavigator} from 'react-navigation-stack';
+import {createAppContainer, createStackNavigator} from 'react-navigation';
+//import {createStackNavigator} from 'react-navigation-stack';
 
 import Contacts from './screens/Contacts';
 import Profile from './screens/Profile';
 
-const StackNavigation=createStackNaavigator(
+const StackNavigation=createStackNavigator(
      {//bu kısma var olan ektranların adı yazılır. ben bunu sadece örnek olsun diye koydum.bu proje için bunun üzerinde ilgilisayfalar için dü zeltme yapılacaktır.
 
          Contacts,
