81df557c5b591cce35d35b6d448e27b5e2823182
hilal <62089994+ayperi-hilal@users.noreply.github.com>
2021-11-18 11:02:21 +0300

    Add files via upload

---
+++ 2-uygulamalar/routes.js
@@ -0,0 +1,14 @@
+import {createAppContainer} from 'react-navigation';
+import {createStackNaavigator} from 'react-navigation-stack';
+
+const StackNavigation=createStackNaavigator(
+     {//bu kısma var olan ektranların adı yazılır. ben bunu sadece örnek olsun diye koydum.bu proje için bunun üzerinde ilgilisayfalar için dü zeltme yapılacaktır.
+
+         Contacts,
+         Profile,
+     },
+     {
+     initialRouteName:'Contacts',//ilk olarak hangi sayfanın gösterilmesi gerektiğini ifade eder.
+    },
+);
+export default createAppContainer(StackNavigation);
